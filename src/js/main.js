b2dedc159e975137d284eca5102deb1ff554e19a
Basovich <frghfqv86>
2020-12-17 14:45:40 +0200

    add animation_stopper.min.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,17 @@
 // assets
 import './assets/polyffils/polyfills-closest';
 import './assets/polyffils/polyfills-customevent';
+import './assets/libs/animation_stopper.min.js';
 import 'core-js/stable/dom-collections/for-each';
 
 // main function
 import {test} from './components/function';
 
 
+// Init Functions
+window.addEventListener('load', onLoadMain);
 
-console.log(test());
+function onLoadMain() {
+    initAnimationStopper();
+    console.log(test());
+}
